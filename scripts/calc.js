633783485903b1b8dc28a1725c660c2a7dd5a822
johnvenkiah <contact@johnvenkiah.com>
2021-09-29 23:27:21 +0000

    add tests and fix code for wrong data types

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,5 +1,13 @@
-function addition() {
-    return 42;
+function addition(a, b) {
+    if (a == "" || b == "") {
+        return 'insufficient data entered';
+    } else if (typeof a != "number" || typeof b != "number") {
+        return 'wrong data type';
+    } else if (a === NaN || b === NaN) {
+        return 'wrong data type';
+    } else {
+        return a + b;
+    }
 }
 
 module.exports = addition;
--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,8 +1,30 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const addition = require("../calc");
 
 describe("Addition function", () => {
     test("should return 42 for 20 + 22", () => {
         expect (addition(20, 22)).toBe(42);
+    })
+    test("should return 62 for 40 + 22", () => {
+        expect (addition(40, 22)).toBe(62);
+    });
+    test("should return 'wrong data type' for 'string' + 22", () => {
+        expect (addition("string", 22)).toBe('wrong data type');
+    })
+    test("should return 'wrong data type' for 22 + 'string'", () => {
+        expect (addition(22, "string")).toBe('wrong data type');
+    })
+    test("should return 'wrong data type' for '22' + 'string'", () => {
+        expect (addition("22", "string")).toBe('wrong data type');
+    })
+    test("should return 'wrong data type' for 'NaN' + 'NaN'", () => {
+        expect (addition(NaN, NaN)).toBe('wrong data type');
+    })
+    test("should return 'insufficient data entered' for '' + ''", () => {
+        expect (addition("", "")).toBe('insufficient data entered');
     })
 });
 
